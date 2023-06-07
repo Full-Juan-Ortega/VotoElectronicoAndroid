e6420e2e9722240123c03e0a7e9e647d68f456c4
Marcos Javier Gómez Höllger <marcosjaviergomezh@gmail.com>
2023-06-07 19:58:31 -0300

    Inicio App Creado
    Creado inicio de app.

--- appVoto/app/src/main/java/com/ispc/ispcappvoto/MainActivity.java
+++ appVoto/app/src/main/java/com/ispc/ispcappvoto/MainActivity.java
@@ -3,6 +3,7 @@ package com.ispc.ispcappvoto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //funcion para desloguear
-        // auth0Service.logout(this);
+         auth0Service.logout(this);
         // funcion para loguear
        //auth0Service.loginWithBrowser(this);
 
 
+    }
+    public void botonClick(View view){
+        auth0Service.loginWithBrowser(this);
+    }
+    public void botonLogout(View view){
+        auth0Service.logout(this);
     }
 }
