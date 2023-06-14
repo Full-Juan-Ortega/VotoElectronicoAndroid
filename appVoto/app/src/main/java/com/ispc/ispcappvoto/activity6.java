1c144e72ffdbde689fdb27a83e208e37813a2d44
Lucas Bartolone <lbartolone@mobydigital.com>
2023-06-13 23:28:16 -0300

    agregados header y footer a activity6

--- appVoto/app/src/main/java/com/ispc/ispcappvoto/activity6.java
+++ appVoto/app/src/main/java/com/ispc/ispcappvoto/activity6.java
@@ -2,7 +2,10 @@ package com.ispc.ispcappvoto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class activity6 extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class activity6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_6);
+
+        //METODOS DEL FOOTER
+
+        ImageView buttonActivity2 = findViewById(R.id.buttonActivity2);
+        buttonActivity2.setOnClickListener(v -> {
+            Intent intent = new Intent(activity6.this, Activity2.class);
+            startActivity(intent);
+        });
+
+        ImageView buttonActivity3 = findViewById(R.id.buttonActivity3);
+        buttonActivity3.setOnClickListener(v -> {
+            Intent intent = new Intent(activity6.this, Activity3.class);
+            startActivity(intent);
+        });
+
+        ImageView buttonActivity5 = findViewById(R.id.buttonActivity5);
+        buttonActivity5.setOnClickListener(v -> {
+            Intent intent = new Intent(activity6.this, Activity5.class);
+            startActivity(intent);
+        });
     }
+    //METODOS DEL HEADER(TOOLBAR)
+
+    // Método para manejar el evento de clic en el botón "Volver"
+    public void onBackPressed(View view) {
+        // Volver a la actividad anterior
+        onBackPressed();
+    }
+
+    // Método para manejar el evento de clic en el botón "Cerrar sesión"
+    public void logout(View view){
+        auth0Service.logout(this);
+    }
+
 }
